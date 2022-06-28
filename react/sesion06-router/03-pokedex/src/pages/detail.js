241565d32b34379048e2b77ea04baa219520153d
Jorge Ramón <jorge.ramon2128@gmail.com>
2022-06-27 22:03:10 -0500

    Ejemplo de pokedex con React Router

---
+++ react/sesion06-router/03-pokedex/src/pages/detail.js
@@ -0,0 +1,29 @@
+import Pokemon from "../components/pokemon";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+function Detail() {
+	const params = useParams();
+	const [pokemon, setPokemon] = useState(null);
+
+	useEffect(function () {
+		fetch("https://bedu-pokemon-api.herokuapp.com/pokemon/" + params.id)
+			.then((response) => response.json())
+			.then(({ data }) => setPokemon(data));
+	}, []);
+
+	return (
+		<div className="vw-100 vh-100 d-flex justify-content-center align-items-center">
+			{pokemon ? (
+				<Pokemon
+					number={pokemon.number}
+					name={pokemon.name}
+					artwork={pokemon.artwork}
+					types={pokemon.types}
+				/>
+			) : null}
+		</div>
+	);
+}
+
+export default Detail;
